a8a2a23f4627dabacdba8143d3c7485080a656bb
victor-val <vval26v@gmail.com>
2024-01-08 11:37:18 +0100

    04- Range y of

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ObservableCreateComponent } from './pages/01-observable-create/01-observable-create.component';
 import { SubscriptionsComponent } from './pages/02-subscriptions/02-subscriptions';
 import { FromComponent } from './pages/03-from/03-from';
+import { RangeOfComponent } from './pages/04-range-y-of/04-range-y-of';
 
 export const routes: Routes = [
     { path: '01', component: ObservableCreateComponent },
     { path: '02', component: SubscriptionsComponent },
     { path: '03', component: FromComponent },
+    { path: '04', component: RangeOfComponent },
   ];
---
+++ src/app/pages/04-range-y-of/04-range-y-of.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { of, range } from 'rxjs';
+import { displayLog } from '../../shared/display-log';
+
+@Component({
+  selector: 'app-03-from',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>03-from works!</p>
+    <div id="log-container"></div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RangeOfComponent implements OnInit {
+  ngOnInit() {
+    //Crear observables a partir de secuencias de datos con of y range
+    const source = of(1, 2, 3, 4, 5, 6);
+    const source2 = of(
+        [1, 2, 3], 
+        "hello world", 
+        { foo: 'bar' }, 
+        function sayHello() {
+        return 'Hi!';
+      });
+
+    
+    const subscription = source.subscribe((data: any) => displayLog(data)); 
+    const subscription2 = source2.subscribe((data: any) => displayLog(data));
+
+    const source3 = range(3,10);
+    const subscription3 = source3.subscribe((data: any) => displayLog(data));
+  }
+}
